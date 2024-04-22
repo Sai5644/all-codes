d1ec809bcbe2b0d3438e3f715a1a8581ed494807
Sai5644 <159700477+Sai5644@users.noreply.github.com>
2024-04-22 16:12:07 +0530

    Create Insert a node at a specific position in a linked list.py

---
+++ Insert a node at a specific position in a linked list.py
@@ -0,0 +1,16 @@
+def insertNodeAtPosition(llist, data, position):
+    # Write your code here
+    newBlock = SinglyLinkedListNode(data)
+    if position == 1:
+        newBlock.next = llist
+        return newBlock
+ 
+    index = 1 
+    curr = llist
+    while index != position :
+        curr = curr.next 
+        index += 1 
+ 
+    newBlock.next = curr.next
+    curr.next = newBlock
+    return llist
