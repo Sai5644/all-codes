cca9b9bfe5edf7ddfb6dd996c9dad2b47124c3bc
Sai5644 <159700477+Sai5644@users.noreply.github.com>
2024-04-24 10:09:06 +0530

    Create string3.py

---
+++ string3.py
@@ -0,0 +1,12 @@
+#3. Write a python program to accept a string and print whether it is a palindrome or not
+ 
+    # malayalam
+    # program 
+ 
+#solution
+word = input()
+reverseWord = word[::-1]
+if word == reverseWord:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
